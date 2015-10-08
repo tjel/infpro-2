8537b2e971917bb7ddf9dff7ee3567053b4b4b69
gkm100 <grzegorzmalkiewicz@hotmail.com>
2015-10-08 12:29:02 +0200

    Poprawka 1

--- area/area.cpp
+++ area/area.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 int main( ) 
 { 
-    float PI = 3.14;                // variables can be initialized during declaration 
-    int rad; 
-    cout<< "Enter the radius"; 
-    cin>>rad; 
-    cout<< "Area of the circle is "<< PI * rad * rad; 
+    float PI = 3.1415;                // variables can be initialized during declaration 
+    float rad; 
+    cout<<"Enter the radius: "; 
+    cin>>rad;
+    if(rad>0){
+    	cout<< "Area of the circle is "<< PI * rad * rad;
+    }else{cout<<"Radius must be > 0!"<<endl;} 
+   
     return 0;
 }  
